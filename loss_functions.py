b45f67c5a81d861e16176fe2777302ca95baeb58
ken <ken@chewbacca.usc.edu>
2022-06-23 09:55:02 -0700

    working version of angles

--- loss_functions.py
+++ loss_functions.py
@@ -145,24 +145,49 @@ def initialize_hji_human_forward(dataset, minWith):
         x,a,b = thetaParse(x,a,b)
         
         # a < x < b, b-a is smaller than pi
-        one = torch.logical_and(torch.logical_and(x < b, (b-a)< np.pi),x-a>0)
+        one_in = torch.logical_and(torch.logical_and(x < b, (b-a)< np.pi),x-a>0)
         # b-a is larger than pi, x < a or x > b)
-        two = torch.logical_and((b-a) > np.pi, torch.logical_or(x<a, x>b))
+        two_in = torch.logical_and((b-a) > np.pi, torch.logical_or(x<a, x>b))
+        
+        # checking periodicity
+        three_in = torch.logical_and(x < a, (x + 2*np.pi) < b)
+        four_in = torch.logical_and(x > b, (x - 2*np.pi) > a)  
 
-        case1 = torch.logical_and(inner,torch.logical_or(one,two))
 
-        return torch.logical_or(torch.logical_and(inner, case1), torch.logical_and(torch.logical_not(inner), torch.logical_not(case1)))
-        #if inner:
-        #    return torch.logical_and(inner,torch.logical_or(one,two))
-        #else
-        #    return torch.logical_and(torch.logical_not(inner),torch.logical_not(torch.logical_or(one,two)))
+        # a < x < b, b-a is greater than pi
+        one_out = torch.logical_and(torch.logical_and(x < b, (b-a)> np.pi),x-a>0)
 
+        # b-a is larger than pi, x < a or x > b)
+        two_out = torch.logical_and((b-a) < np.pi, torch.logical_or(x<a, x>b))
+        # checking periodicity
+        three_out = torch.logical_and(x < a, (x + 2*np.pi) < (b)) 
+        four_out = torch.logical_and(x > b, (x - 2*np.pi) > (a))
+
+        case1 = torch.logical_or(torch.logical_or(torch.logical_or(one_in, two_in), three_in),four_in)
+        case2 = torch.logical_or(torch.logical_or(torch.logical_or(one_out, two_out), three_out),four_out)
+
+        return torch.where(inner, case1, case2)
+       
     def pointDist(x,a,b):
         # x is point out of range
         # a is lower bound and b is upper bound
         # return true if closer to a, false otherwise
         x, a, b = thetaParse(x,a,b)
-        return torch.where(x<b/2, True, False)
+        x = x% (2*np.pi)
+        a = a% (2*np.pi)
+        b = b% (2*np.pi)
+
+        x_a1 = torch.abs(x - a)
+        x_b1 = torch.abs(x - b)
+        x_a2 = 2*np.pi - x_a1
+        x_b2 = 2*np.pi - x_b1
+
+        x_a = torch.where(x_a1<x_a2, x_a1, x_a2)
+        x_b = torch.where(x_b1<x_b2, x_b1, x_b2)
+
+        
+        return torch.where(x_a < x_b, True, False)
+  
 
     def thetaParse(x,a,b):
         #a = torch.where(a< -np.pi, a + 2*np.pi, a)
@@ -180,10 +205,10 @@ def initialize_hji_human_forward(dataset, minWith):
         #b = b - a
         #x = x - a
         return x, a, b
-
+ 
     def hji_human_forward(model_output, gt):
         source_boundary_values = gt['source_boundary_values']
-        x = model_output['model_in']  # (meta_batch_size, num_points, 4)
+        x = model_output['model_in']  # (meta_batch_size, num_points, 5)
         y = model_output['model_out']  # (meta_batch_size, num_points, 1)
         dirichlet_mask = gt['dirichlet_mask']
         batch_size = x.shape[1]
@@ -199,7 +224,7 @@ def initialize_hji_human_forward(dataset, minWith):
         # \dot y0   = 0
         # \dot p    = 0
         
-        u_star = torch.atan2(-goal[1]+x[...,4], -goal[0]+x[...,3]) # angle directly to goal
+        u_star = torch.atan2(-goal[1]+x[...,2], -goal[0]+x[...,1]) # angle directly to goal
 
         # control that maximizes the hamiltonians
         control1 = torch.atan2(dudx[...,1], dudx[...,0])
